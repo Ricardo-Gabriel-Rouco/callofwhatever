780cfa1eab9dd64b204ae57b1a4e1bbd95ab616d
Gabriel Rouco <r.g.rouco1@gmail.com>
2022-10-20 16:57:45 -0300

    "agregue clase monstruo epico"

---
+++ personajes/jefes.js
@@ -0,0 +1,9 @@
+class monstruosEpicos {
+    constructor(){
+        this.nivel = 0;
+        this.vida = 1000;
+        this.dañofisico = 0;
+        this.dañomagico = 0;
+        this.dificultad = 0;
+    }
+}
