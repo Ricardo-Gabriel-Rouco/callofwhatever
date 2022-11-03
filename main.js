65b874a8d747d774ec6640ef4d662860a4eadaa6
Gabriel Rouco <r.g.rouco1@gmail.com>
2022-11-02 22:28:07 -0300

    "agregado de una parte dle mapeado"

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const canvas = document.querySelector('canvas')
+var canvas = document.getElementById('main-canvas')
+var c = canvas.getContext('2d')
+canvas.width = 640;
+canvas.height = 480;
 
+var image = new Image()
+image.src ='./assets/prototipomapa.png'
+c.fillRect(0, 0 , canvas.width, canvas.height)
+
+image.onload = () => {
+    c.drawImage(image, -1000, -460)
+}
