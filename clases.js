6789fbb494409cc1ff70721621438cd4327f04a5
Gabriel Rouco <r.g.rouco1@gmail.com>
2022-11-12 19:21:02 -0300

    ""

--- clases.js
+++ clases.js
@@ -1,9 +1,9 @@
 // con esto se definen todas las texturas
-class texturas{
-    constructor({posicion, velocidad, imagen, frames = {max: 1}, sprites}){
+class texturas {
+    constructor({ posicion, velocidad, imagen, frames = { max: 1 }, sprites }) {
         this.posicion = posicion
         this.imagen = imagen
-        this.frames = {...frames, val: 0, elapsed: 0}
+        this.frames = { ...frames, val: 0, elapsed: 0 }
         this.sprites = sprites
         this.imagen.onload = () => {
             this.width = this.imagen.width / this.frames.max
@@ -22,15 +22,15 @@ class texturas{
             this.imagen.width / this.frames.max,
             this.imagen.height,
             this.posicion.x,
-            this.posicion.y,              
+            this.posicion.y,
             this.imagen.width / this.frames.max,
-            this.imagen.height )
-        if(this.moving){
-            if(this.frames.max > 1){
+            this.imagen.height)
+        if (this.moving) {
+            if (this.frames.max > 1) {
                 this.frames.elapsed++
             }
-            if(this.frames.elapsed % 10 === 0){
-                if(this.frames.val < this.frames.max - 1) this.frames.val++
+            if (this.frames.elapsed % 10 === 0) {
+                if (this.frames.val < this.frames.max - 1) this.frames.val++
                 else this.frames.val = 0
             }
         }
@@ -38,16 +38,16 @@ class texturas{
 }
 
 //esta es la clase que define las colisiones o limites
-class Limite{ //BOundary
+class Limite { //BOundary
     static ancho = 36
     static alto = 36
-    constructor({posicion}){
+    constructor({ posicion }) {
         this.posicion = posicion
         this.ancho = 36
         this.alto = 36
     }
 
-    draw(){
+    draw() {
         c.fillStyle = 'rgba(255, 0 , 0 , 0)'
         c.fillRect(this.posicion.x, this.posicion.y, this.ancho, this.alto)
     }
